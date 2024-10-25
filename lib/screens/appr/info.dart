545ed59ed71b5c2e01f8533146fdad020d38e0df
HwangSangSeob <45357279+HwangSangSub@users.noreply.github.com>
2024-10-25 17:43:16 +0900

    전자결재문서상세PDF 구현

--- lib/screens/appr/info.dart
+++ lib/screens/appr/info.dart
@@ -1,10 +1,10 @@
+import 'dart:convert';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:flutter_html_table/flutter_html_table.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
+import 'package:pdfx/pdfx.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_html/flutter_html.dart';
-import 'dart:convert';
 import '../../provider/loginProvider.dart';
 
 class ApprInfoPage extends StatefulWidget {
@@ -17,16 +17,25 @@ class ApprInfoPage extends StatefulWidget {
 }
 
 class _ApprInfoPageState extends State<ApprInfoPage> {
-  Map<String, dynamic>? appDetail;
+  late Map<String, dynamic> appDetail = {};
   late int empNo;
+  PdfControllerPinch? pdfController; // PDF Controller
 
   @override
   void initState() {
+    WidgetsFlutterBinding.ensureInitialized(); // Flutter 위젯 초기화 보장
     super.initState();
     empNo = Provider.of<LoginProvider>(context, listen: false).empNo!;
-    fetchScheduleDetail();
+    fetchScheduleDetail(); // API 호출 시작
   }
 
+  @override
+  void dispose() {
+    pdfController?.dispose(); // PdfController 해제
+    super.dispose();
+  }
+
+  /// API에서 스케줄 정보를 가져오고 PDF를 초기화합니다.
   Future<void> fetchScheduleDetail() async {
     final url = Uri.parse(
       'http://192.168.0.40:8099/api/appInfo?empNo=$empNo&eaNo=${widget.eaNo}',
@@ -34,10 +43,18 @@ class _ApprInfoPageState extends State<ApprInfoPage> {
 
     try {
       final response = await http.get(url);
+
       if (response.statusCode == 200) {
         final data = jsonDecode(utf8.decode(response.bodyBytes));
         setState(() {
           appDetail = data;
+        });
+        final pdfBytes = decodePdfContent(appDetail['pdfInfo']);
+
+        setState(() {
+          pdfController = PdfControllerPinch(
+            document: PdfDocument.openData(pdfBytes),
+          );
         });
       } else {
         print('오류 발생: ${response.statusCode}');
@@ -47,6 +64,19 @@ class _ApprInfoPageState extends State<ApprInfoPage> {
     }
   }
 
+  /// PDF 콘텐츠를 Uint8List로 변환합니다.
+  Uint8List decodePdfContent(dynamic content) {
+    if (content is String) {
+      // Base64 문자열을 디코딩
+      return base64.decode(content);
+    } else if (content is Uint8List) {
+      return content;
+    } else {
+      throw Exception('알 수 없는 PDF 데이터 형식');
+    }
+  }
+
+  /// 날짜 포맷팅 함수
   String formatDateTime(String? dateTimeString) {
     if (dateTimeString == null || dateTimeString.isEmpty) {
       return '알 수 없음';
@@ -61,58 +91,59 @@ class _ApprInfoPageState extends State<ApprInfoPage> {
       appBar: AppBar(
         title: const Text('전자결재상세'),
       ),
-      body: appDetail == null
-          ? const Center(child: CircularProgressIndicator())
-          : Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Card(
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Center(
-                          child: Text(
-                            appDetail!['title'] ?? '제목 없음',
-                            style: const TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        _buildInfoRow(
-                          icon: Icons.calendar_today,
-                          label: '기안일',
-                          value: formatDateTime(appDetail!['regDate']),
-                        ),
-                        const SizedBox(height: 10),
-                        if (appDetail!['compDate'] != null &&
-                            appDetail!['compDate'].toString().isNotEmpty)
-                          _buildInfoRow(
-                            icon: Icons.calendar_today_outlined,
-                            label: '결재종료일',
-                            value: formatDateTime(appDetail!['compDate']),
-                          ),
-                        const SizedBox(height: 20),
-                        const Divider(),
-                        const SizedBox(height: 10),
-                        Html(
-                          data: cleanEscapedHtml(
-                              appDetail!['content'] ?? '<p>내용없음</p>'),
-                          extensions: const [TableHtmlExtension()],
-                        ),
-                      ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          elevation: 4,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Text(
+                      appDetail['title'] ?? '제목 없음',
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
+                  const SizedBox(height: 20),
+                  _buildInfoRow(
+                    icon: Icons.calendar_today,
+                    label: '기안일',
+                    value: formatDateTime(appDetail['regDate']),
+                  ),
+                  const SizedBox(height: 10),
+                  if (appDetail['compDate'] != null &&
+                      appDetail['compDate'].toString().isNotEmpty)
+                    _buildInfoRow(
+                      icon: Icons.calendar_today_outlined,
+                      label: '결재종료일',
+                      value: formatDateTime(appDetail['compDate']),
+                    ),
+                  const SizedBox(height: 20),
+                  const Divider(),
+                  const SizedBox(height: 10),
+                  pdfController == null
+                      ? const Center(child: CircularProgressIndicator())
+                      : SizedBox(
+                          height: 500,
+                          child: PdfViewPinch(
+                            controller: pdfController!,
+                          ),
+                        ),
+                ],
               ),
             ),
+          ),
+        ),
+      ),
     );
   }
 
@@ -151,11 +182,5 @@ class _ApprInfoPageState extends State<ApprInfoPage> {
         ),
       ],
     );
-  }
-
-  String cleanEscapedHtml(String html) {
-    String cleaned = html.replaceAll(RegExp(r'[\r\n\t]+'), ' ');
-    cleaned = cleaned.replaceAll(r'\"', '"');
-    return cleaned.trim();
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
+import pdfx
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  PdfxPlugin.register(with: registry.registrar(forPlugin: "PdfxPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
